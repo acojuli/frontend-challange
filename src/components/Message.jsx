67c9d114658a0cc451296c717ce5b28d6106456a
Julian Gomez <acojuli@gmail.com>
2021-12-09 07:50:48 -0300

    Message and PlayerStatus components

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 /*
  * The Message component contains an anchor element and
  * a paragraph below the anchor. Rendering of the paragraph
@@ -10,6 +9,7 @@ import { useState } from 'react'
  * - After another click, the paragraph should not be rendered.
  * - Finish the Message component by implementing this logic.
  */
+import { useState } from 'react';
 
 export function Message() {
 	const [isAvailable, setIsAvailable] = useState(false);
@@ -20,7 +20,7 @@ export function Message() {
 	return (
 		<>
 			<a href="#" onClick={handleClick}>Want to buy a new car?</a>
-				{isAvailable && <p>Call +11 22 33 44 now!</p>}
+			{isAvailable && <p>Call +11 22 33 44 now!</p>}
 		</>
 	);
 }
