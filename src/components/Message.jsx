212c342538661fc43d5d26b5c08aaefd99904d25
Julian Gomez <acojuli@gmail.com>
2021-12-09 07:46:44 -0300

    first commit

---
+++ src/components/Message.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+/*
+ * The Message component contains an anchor element and
+ * a paragraph below the anchor. Rendering of the paragraph
+ * should be toggled by clicking on the anchor element
+ * using the following logic:
+ *
+ * - At the start, the paragraph should not be rendered.
+ * - After a click, the paragraph should be rendered.
+ * - After another click, the paragraph should not be rendered.
+ * - Finish the Message component by implementing this logic.
+ */
+
+export function Message() {
+	const [isAvailable, setIsAvailable] = useState(false);
+	const handleClick = () => {
+		setIsAvailable(!isAvailable);
+	};
+
+	return (
+		<>
+			<a href="#" onClick={handleClick}>Want to buy a new car?</a>
+				{isAvailable && <p>Call +11 22 33 44 now!</p>}
+		</>
+	);
+}
